c3da935debec1b16437316436f52ff74e3acac8f
Karthik RP <blendedidea@gmail.com>
2022-02-13 11:19:07 +0530

    Husky Check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
-const mongoose = require('./config/mongoose');
+const mongoose = require("./config/mongoose");
 
 // open mongoose connection
 mongoose.connect();
